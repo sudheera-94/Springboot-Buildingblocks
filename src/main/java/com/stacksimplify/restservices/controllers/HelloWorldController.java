c7542b1729c6fb9341c103d47c2f0837b28c84e1
Sudheera Withanage <sudheerawithanage@gmail.com>
2021-09-20 16:05:26 +0530

    Hello World base

---
+++ src/main/java/com/stacksimplify/restservices/controllers/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.stacksimplify.restservices.controllers;
+
+import com.stacksimplify.restservices.dto.UserDetailsDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@RestController
+public class HelloWorldController {
+
+    // Simple method
+    // URI - /helloWorld
+    // Method - GET
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/userDetails")
+    public UserDetailsDto getUserDetails() {
+        return new UserDetailsDto("Sudheera", "Withanage", "Colombo");
+    }
+}
---
+++ src/main/java/com/stacksimplify/restservices/dto/UserDetailsDto.java
@@ -0,0 +1,17 @@
+package com.stacksimplify.restservices.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDetailsDto {
+
+    private String firstName;
+    private String lastName;
+    private String city;
+
+    public UserDetailsDto(String firstName, String lastName, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+}
